85bf834009d1fec30ffbf6fbb3a4058025934ab5
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2026-03-11 09:40:15 +0200

    Create template

---
+++ sendMentorInvites.ts
@@ -0,0 +1,64 @@
+import { bulkSendHT } from './bulkSendHT';
+import { HackTUESEmailOptions } from './sendEmail';
+
+/**
+ * Main function to send declaration emails
+ */
+async function sendMentorInvites() {
+  try {
+    // Define email options
+    const emailOptions: Omit<HackTUESEmailOptions, 'to'> = {
+      subject: 'Стар Уорс',
+      text: `Здравейте,
+
+      Благодарим, че ще бъдете ментор на Hack TUES 12! Без Вас събитието не би било същото!
+
+      На 17 март (вторник) от 20:30 ще се проведе онлайн информационна среща за всички ментори, на която ще Ви запознаем с необходимата информация за предстоящото събитие. В случай, че нямате възможност да се включите, ще направим запис от срещата и ще Ви го изпратим. Линк към срещата може да намерите тук:
+      https://meet.google.com/ikp-syic-rgd
+
+      Молим Ви да попълните анкета за информирано съгласие (<strong>в срок до 16 март</strong>):
+      https://docs.google.com/forms/d/e/1FAIpQLScupMXOUoGLWplz-sjqEMjPqdSixRih2uMH0kJ7Rcsv46qTcw/viewform
+
+      Отделно ще се радваме, ако се запознаете с наръчника на ментора чрез следния survival guide:
+      https://docs.google.com/document/d/1d-mq7hqDUZPCh3DkE96dlyckHsXq8AriQKLNANfpLzA/edit?usp=sharing
+
+      При въпроси или нужда от съдействие оставаме на разположение. 
+
+      Очакваме Ви с нетърпение!
+
+      Поздрави,
+      Екипът на Hack TUES 12`,
+      html: `<div>Здравейте,
+
+      <p>Благодарим, че ще бъдете ментор на Hack TUES 12! Без Вас събитието не би било същото!</p>
+
+      <p>На <strong>17 март (вторник)</strong> от <strong>20:30</strong> ще се проведе онлайн информационна среща за всички ментори, на която ще Ви запознаем с необходимата информация за предстоящото събитие. В случай, че нямате възможност да се включите, ще направим запис от срещата и ще Ви го изпратим. Линк към срещата може да намерите <a href="https://meet.google.com/ikp-syic-rgd">тук</a>.</p>
+
+      <p>Молим Ви да попълните анкета за информирано съгласие (в срок до 16 март) <a href="https://docs.google.com/forms/d/e/1FAIpQLScupMXOUoGLWplz-sjqEMjPqdSixRih2uMH0kJ7Rcsv46qTcw/viewform?usp=dialog">тук</a>.</p>
+
+      <p>Отделно ще се радваме, ако се запознаете с наръчника на ментора чрез следния <a href="https://docs.google.com/document/d/1d-mq7hqDUZPCh3DkE96dlyckHsXq8AriQKLNANfpLzA/edit?usp=sharing">survival guide</a>.</p>
+
+      <p>При въпроси или нужда от съдействие оставаме на разположение.</p>
+
+      <p>Очакваме Ви с нетърпение!</p>
+
+      <p>Поздрави,<br/>
+      Екипът на Hack TUES 12</p>
+</div>`,
+      fromName: "team",
+    };
+
+    // Call bulkSendHT with the email options
+    await bulkSendHT(emailOptions);
+    
+  } catch (error: any) {
+    console.error('\n=== Error ===');
+    console.error('Error:', error.message);
+    process.exit(1);
+  } finally {
+    process.exit(0);
+  }
+}
+
+// Run the function
+sendMentorInvites();
